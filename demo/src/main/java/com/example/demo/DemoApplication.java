2d49cc30e1703b3248ec47428c6a9ce399641dc4
dinhquangkhuong <113097235+dinhquangkhuong@users.noreply.github.com>
2023-03-16 21:03:19 +0700

    huong dan dung

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,29 @@
 package com.example.demo;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/*
+	vao link: http://localhost:8080/hello
+	sau khi chay code
+
+ */
 
 @SpringBootApplication
+@RestController
 public class DemoApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
+	@GetMapping("/hello")
+	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return String.format("Bye %s!", name);
+	}
 
+	@GetMapping("/login")
+	public String login(@RequestParam(value = "login", defaultValue = "no user") String login) {
+		return String.format("please login");
+	}
 }
